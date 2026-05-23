a5635a643b8c0d923a74c87d95413acfb38313cd
Sumon Ahmed <sa.cse.2k23@gmail.com>
2026-05-23 08:57:29 +0600

    Implement dynamic display for 7-segment tube

--- 8051_Dynamic_Digital_Tube_Display.c
+++ 8051_Dynamic_Digital_Tube_Display.c
@@ -1,1 +1,99 @@
+//8051 Dynamic Digital Tube Display
+#include "reg52.h"
+// Data type define
+typedef unsigned int u16;
+typedef unsigned char u8;
 
+// 74HC138 control pins
+sbit LSA = P2^2;
+sbit LSB = P2^3;
+sbit LSC = P2^4;
+
+// 7-segment display codes (0-F)
+u8 code smgduan[17] =
+{
+    0x3f, //0
+    0x06, //1
+    0x5b, //2
+    0x4f, //3
+    0x66, //4
+    0x6d, //5
+    0x7d, //6
+    0x07, //7
+    0x7f, //8
+    0x6f, //9
+    0x77, //A
+    0x7c, //b
+    0x39, //C
+    0x5e, //d
+    0x79, //E
+    0x71  //F
+};
+
+// Delay function
+void delay(u16 i)
+{
+    while(i--);
+}
+
+// Dynamic display function
+void DigDisplay()
+{
+    u8 i;
+
+    for(i=0; i<8; i++)
+    {
+        switch(i)
+        {
+            case(0):
+                LSA=0; LSB=0; LSC=0;
+                break;
+
+            case(1):
+                LSA=1; LSB=0; LSC=0;
+                break;
+
+            case(2):
+                LSA=0; LSB=1; LSC=0;
+                break;
+
+            case(3):
+                LSA=1; LSB=1; LSC=0;
+                break;
+
+            case(4):
+                LSA=0; LSB=0; LSC=1;
+                break;
+
+            case(5):
+                LSA=1; LSB=0; LSC=1;
+                break;
+
+            case(6):
+                LSA=0; LSB=1; LSC=1;
+                break;
+
+            case(7):
+                LSA=1; LSB=1; LSC=1;
+                break;
+        }
+
+        // Send display code
+        P0 = smgduan[i];
+
+        // Small delay for scanning
+        delay(100);
+
+        // Clear display
+        P0 = 0x00;
+    }
+}
+
+// Main function
+void main()
+{
+    while(1)
+    {
+        DigDisplay();
+    }
+}
